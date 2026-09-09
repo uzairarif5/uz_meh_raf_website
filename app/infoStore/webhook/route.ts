bd75ecdf9879671125e0b4f7afc98ed3ebdd9dd0
Uzair <uzair.99@hotmail.com>
2026-09-09 19:40:38 +0400

    updated 0.11.1

--- app/infoStore/webhook/route.ts
+++ app/infoStore/webhook/route.ts
@@ -1,10 +1,20 @@
 export async function POST(request: Request) {
   const body = await request.json();
 
-  console.log(body);
-  
-  return new Response(JSON.stringify({ success: "true"}), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' }
-  });
+  try{
+    console.log(body["head_commit"]);
+    
+    return new Response(JSON.stringify({ success: true}), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+  }
+  catch (err) {
+    console.error(err);  
+    return new Response(JSON.stringify({ success: false}), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  }
 }
